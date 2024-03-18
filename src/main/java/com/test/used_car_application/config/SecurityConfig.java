3599378fa808fb91df96a7aa712869fc6c44f3b0
Kaszás Viktor <kaszas.viktor1993@gmail.com>
2024-03-18 09:26:52 +0100

    Plus.disable() for Spring Security config

--- src/main/java/com/test/used_car_application/config/SecurityConfig.java
+++ src/main/java/com/test/used_car_application/config/SecurityConfig.java
@@ -39,8 +39,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
      @Override
      protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().and()
-                .authorizeRequests()
+        http.csrf().disable().authorizeRequests()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
